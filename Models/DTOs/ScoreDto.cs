4a2bcf36005d7f84ec606775f9a975c136b33030
cnpl <bhuvana@cnpl.net>
2025-09-20 18:06:13 +0530

    Update ScoreDto.cs

--- Models/DTOs/ScoreDto.cs
+++ Models/DTOs/ScoreDto.cs
@@ -1,1 +1,7 @@
+using System.ComponentModel.DataAnnotations;
 
+namespace ScoreTracker.Api.Models.DTOs;
+
+public record ScoreDto(int Id, int Value, DateTime DateRecorded);
+
+public record CreateScoreDto([Required] int Value);
