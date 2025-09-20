179c5d918eee6714ff966cfb65f7f3621b6e698b
cnpl <bhuvana@cnpl.net>
2025-09-20 19:02:36 +0530

    Update UsersController.cs

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,1 +1,56 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ScoreTracker.Api.Models.DTOs;
+using ScoreTracker.Api.Models.Entities;
 
+namespace ScoreTracker.Api.Controllers;
+
+[Authorize(Roles = "Admin")] // This entire controller is protected for Admins only
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public UsersController(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    // GET: api/users - Gets a list of all users
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<UserDto>>> GetUsers()
+    {
+        var users = await _userManager.Users.ToListAsync();
+        var userDtos = new List<UserDto>();
+
+        foreach (var user in users)
+        {
+            var roles = await _userManager.GetRolesAsync(user);
+            userDtos.Add(new UserDto(user.Id, user.FullName ?? "", user.Email ?? "", roles));
+        }
+
+        return Ok(userDtos);
+    }
+
+    // DELETE: api/users/{id} - Deletes a user
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(string id)
+    {
+        var user = await _userManager.FindByIdAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var result = await _userManager.DeleteAsync(user);
+        if (!result.Succeeded)
+        {
+            return BadRequest(result.Errors);
+        }
+
+        return NoContent();
+    }
+}
