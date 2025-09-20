22be9da347d876b94ed516bf2d03e803b4ab3fd4
cnpl <bhuvana@cnpl.net>
2025-09-20 18:03:52 +0530

    Update Score.cs

--- Models/Entities/Score.cs
+++ Models/Entities/Score.cs
@@ -1,1 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace ScoreTracker.Api.Models.Entities;
+
+public class Score
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public int Value { get; set; }
+
+    [Required]
+    public DateTime DateRecorded { get; set; }
+
+    // Foreign Key to the ApplicationUser
+    [Required]
+    public string UserId { get; set; } = null!;
+
+    [ForeignKey(nameof(UserId))]
+    public virtual ApplicationUser User { get; set; } = null!;
+}
